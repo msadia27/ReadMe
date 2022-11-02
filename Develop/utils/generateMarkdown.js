67598296644e6960705c88698aaa849e99c079e1
Madelane Sadia <madelanecsadia@gmail.com>
2022-11-02 04:17:11 -0400

    install inquirer

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -11,6 +11,8 @@
 //function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+
+
 function generateMarkdown(data) {
   return `
   # ${data.title} ![Githublicense](http://img.shields.io/badge/license-${data.license}-blue.svg);
