7c9ef86ec27073b78fb85480b14497fb53de17ac
Madelane Sadia <madelanecsadia@gmail.com>
2022-11-07 21:55:53 -0500

    final commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,77 +1,70 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const fs = require('fs');
-const generateMarkdown = require("../utils/generateMarkdown");
-
+const fs = require("fs");
+const { generateMarkdown } = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt ([
-    {
+const questions = [
+  {
     type: "input",
     name: "title",
-    message: "What is the title of your project?"
-    },
-    {
+    message: "What is the title of your project?",
+  },
+  {
     type: "input",
     name: "description",
-    message: "Please enter a description for your project."
-    },
-    {
+    message: "Please enter a description for your project.",
+  },
+  {
     type: "input",
     name: "installation",
-    message: "What are the instructions for installation of your project?"
-    },
-    {
+    message: "What are the instructions for installation of your project?",
+  },
+  {
     type: "input",
     name: "usage",
-    message: "How do you use your project?"
-    },
-    {
+    message: "How do you use your project?",
+  },
+  {
     type: "input",
     name: "licenses",
     message: "What kind of licenses your project need?",
-    choices: ["GNU", "Apache", "MIT", "Mozilla"
-    ]
-    },
-    {
+    choices: ["GNU", "Apache", "MIT", "Mozilla"],
+  },
+  {
     type: "input",
     name: "contributions",
-    message: "How can user contribute to your project?"
-    },
-    {
+    message: "How can user contribute to your project?",
+  },
+  {
     type: "input",
     name: "github",
-    message: "What is your github username?"
-    },
-    {
+    message: "What is your github username?",
+  },
+  {
     type: "input",
     name: "email",
-    message: "What is your email address?"
-    }
-])
-};
+    message: "What is your email address?",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function(err){
-        console.log(data)
-        if(err){
-            console.log(err)
-        } else {
-            console.log("You have successfully created your README!")
-        }
-    });
-};
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("You have successfully created your README!");
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
-    .then(function(data){
-        writeToFile("README.md", generateMarkdown(data))
-        console.log(data)
-    })
-};
+  inquirer.prompt(questions).then(function (data) {
+    writeToFile("README.md", generateMarkdown(data));
+  });
+}
 
 // Function call to initialize app
 init();
